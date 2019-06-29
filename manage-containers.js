27188e82e23ff1fc6b07eb3ae13c2d3b300b680f
ktsesmetzidis <ktsesmetzidis@impedimed.com>
2019-06-29 12:31:01 +0300

    Manage containers
    - Create a new file named manage-containers
    - Crete one or multiple containers
    - Start/Stop containers concurrently
    - Retrieve Stats/Logs from a specific one

---
+++ manage-containers.js
@@ -0,0 +1,72 @@
+const {Docker}  = require('node-docker-api');
+const docker    = new Docker({ socketPath: '/var/run/docker.sock' });
+
+const {fetchAll, fetchRun, fetchIdle} = require('./view-containers');
+
+let inputParams = [
+    {
+        name: 'test-elixir',
+        Image: 'elixir'
+    },
+    {
+        name: 'test-mysql',
+        Image: 'mysql',
+        AttachStdin: false,
+        AttachStdout: true,
+        AttachStderr: true,
+        Env: [
+            'MYSQL_ROOT_PASSWORD=administrator' // todo
+        ]
+    },
+    {
+        name: 'test-hello',
+        Image: 'hello-world'
+    }
+];
+
+const parallel = tasks => Promise.all(tasks.map(task => task()))
+
+const create = input => Promise.resolve(input)
+    .then(arr => arr.map(opts => () => docker.container.create(opts)))
+    .then(tasks => parallel(tasks))
+    .catch(err => console.log(err))
+
+const start = () => fetchIdle()
+    .then(containers => containers.map(container => () => container.start()))
+    .then(tasks => parallel(tasks))
+    .catch(err => console.log(err))
+
+const stop = () => fetchRun()
+    .then(containers => containers.map(container => () => container.stop()))
+    .then(tasks => parallel(tasks))
+    .catch(err => console.log(err))
+
+const logs = () => fetchAll()
+    .then(containers => containers[0].logs({ follow: true, stdout: true, stderr: true }))
+    .then(stream => {
+        stream.on('data', info => console.log(info.toString()))
+        stream.on('error', err => console.log(err))
+    })
+    .catch(err => console.log(err));
+
+const stats = () => fetchAll()
+    .then(containers => containers[0].status())
+    .then(container => container.stats())
+    .then(stats => {
+        stats.on('data', stat => console.log('Stats: ', JSON.parse(stat)))
+        stats.on('error', err => console.log('Error: ', err))
+    })
+    .catch(error => console.log(error));
+
+
+module.exports = {
+    create: create.bind(null, inputParams),
+    start,
+    stop,
+    logs,
+    stats
+};
+
+
+
+
