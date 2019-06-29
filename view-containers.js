f163b30585d536b59da9514fd70615f1d6994489
ktsesmetzidis <ktsesmetzidis@impedimed.com>
2019-06-29 12:28:37 +0300

    Get containers' info
    - Create a new file named view-containers
    - View all or running containers
    - Fetch all, running or idle containers

---
+++ view-containers.js
@@ -0,0 +1,25 @@
+const {exec}    = require("child_process");
+const {Docker}  = require('node-docker-api');
+const docker    = new Docker({socketPath: '/var/run/docker.sock'});
+
+const list = opts => docker.container.list(opts);
+
+const idle = () => list({all: true})
+    .then(containers =>
+        containers.filter(container => container['data']['State'] !== 'running'));
+
+const view = command =>
+    new Promise((resolve, reject) =>
+        exec(command, (err, stdout, stderr) =>
+            err ? reject(stderr) : resolve(stdout)));
+
+
+module.exports = {
+    viewAll : view.bind(null, 'sudo docker ps -a'),
+    viewRun : view.bind(null, 'sudo docker ps'),
+    fetchAll: list.bind(null, {all: true}),
+    fetchRun: list.bind(null, {}),
+    fetchIdle: idle
+};
+
+
